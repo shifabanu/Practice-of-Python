9f3e55b6034d6b9c35161ca307fcf425f74c20b0
Shifa Shaikh <70238196+shifabanu@users.noreply.github.com>
2021-09-14 12:19:07 -0700

    Create largest_number.py

---
+++ arrays/largest_number.py
@@ -0,0 +1,26 @@
+'''
+
+Find the largest number without using built in function
+
+'''
+
+array = [] #create an empty array
+
+length = int(input("Length of your array is: "))
+
+for i in range(0, length):
+    number = int(input("Enter the element of the array: "))
+    array.append(number)
+
+print("You entered: ", array)
+
+#find the largest among the array
+
+max = array[0]
+
+for j in range(0, length):
+    if array[j] > max:
+        max = array[j]
+
+print("The largest number is: ", max)
+
