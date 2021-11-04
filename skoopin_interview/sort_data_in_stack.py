86b54ef2ec9f9ffe06c6115b2dff512b381db3f9
Shifa Shaikh <70238196+shifabanu@users.noreply.github.com>
2021-11-04 10:56:47 -0700

    Create sort_data_in_stack.py

---
+++ skoopin_interview/sort_data_in_stack.py
@@ -0,0 +1,13 @@
+'''
+stack.push_back()
+stack.pop()
+stack.top()
+stack.empty()
+And can use only 1 variable to store. 
+
+How will you sort them
+'''
+
+'''
+Interviewer agreed on using recursion
+'''
